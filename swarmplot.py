72ee254ebca01a8094504bbebad8cfce35db9c64
ewenorsal.1 <ewenorsal.1@gmail.com>
2024-04-03 14:39:29 -0400

    Optimized a little bit + added color choice

--- swarmplot.py
+++ swarmplot.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 import numpy as np
 
-def swarmPlot(x, size, figSize=(1000, 500), xmin=0, xmax=4, ymin=-25, ymax=25, ystep=1, seed=0):
+def swarmPlot(x, size, figSize=(1000, 500), xmin=0, xmax=4, ymin=-25, ymax=25, ystep=0.5, color='#a86022', seed=0):
     np.random.seed(seed)
     ratio = figSize[0]*(ymax-ymin)/(figSize[1]*(xmax-xmin))
     def isInEllipse(x, y, x0, y0, r1, r2):
@@ -32,7 +32,7 @@ def swarmPlot(x, size, figSize=(1000, 500), xmin=0, xmax=4, ymin=-25, ymax=25, y
     for xi, ri in zip(x, size):
         yi = 0
         dir = np.random.choice([1, -1])
-        closeIndexes = indexes[np.abs(x - xi) < rmax]
+        closeIndexes = indexes[np.abs(x - xi) < 2*rmax/ratio]
         closeIndexes = closeIndexes[closeIndexes < len(y)]
         xClose = x[closeIndexes] if len(closeIndexes) > 0  else []
         yClose = np.array(y)[closeIndexes] if len(closeIndexes) > 0  else []
@@ -42,7 +42,7 @@ def swarmPlot(x, size, figSize=(1000, 500), xmin=0, xmax=4, ymin=-25, ymax=25, y
         y.append(yi)
 
     fig = go.Figure()
-    kwargs = {'type': 'circle', 'xref': 'x', 'yref': 'y', 'fillcolor': '#a86022'}
+    kwargs = {'type': 'circle', 'xref': 'x', 'yref': 'y', 'fillcolor': color}
     points = [go.layout.Shape(x0=x-r/ratio, y0=y-r, x1=x+r/ratio, y1=y+r, **kwargs) for x, y, r in zip(x, y, size)]
     fig.update_layout(shapes=points, width=figSize[0], height=figSize[1], margin=dict(l=20, r=20, t=20, b=20))
     fig.update_xaxes(range=[xmin, xmax])  
