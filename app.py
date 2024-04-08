efba2dbec963c8f5ee574860f0ff6a2536505292
pierr <pierr@LAPTOP-KQRQM3A6>
2024-04-08 00:37:13 -0400

    add vizualisations with interactivity

--- app.py
+++ app.py
@@ -6,11 +6,16 @@ import json
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output
 
 import pandas as pd
 import numpy as np
 
 import preprocess
+import choropleth
+import arrond_map
+import bar_chart
+import swarmplot
 # import bubble
 
 app = dash.Dash(__name__)
@@ -20,34 +25,41 @@ server = app.server
 
 species = preprocess.getSpeciesList()
 
-# with open('../src/assets/data/countriesData.json') as data_file:
-#     data = json.load(data_file)
+# Fichier geojson pour le choropleth
+with open('assets/montreal.json', encoding='utf-8') as data_file:
+    montreal_data = json.load(data_file)
 
-# df_2000 = pd.json_normalize(data, '2000')
-# df_2015 = pd.json_normalize(data, '2015')
+# Fichier contenant les données sur les arbres
+data = pd.read_csv('assets/arbres-publics.csv')
 
-# df_2000 = preprocess.round_decimals(df_2000)
-# df_2015 = preprocess.round_decimals(df_2015)
+data = preprocess.preprocess_df(data)
+data = preprocess.removeOutliers(data)
+locations = preprocess.get_neighborhoods(montreal_data)
 
-# gdp_range = preprocess.get_range('GDP', df_2000, df_2015)
-# co2_range = preprocess.get_range('CO2', df_2000, df_2015)
+date_plantation_min = data['Date_plantation'].min()
+date_plantation_max = data['Date_plantation'].max()
 
-# df = preprocess.combine_dfs(df_2000, df_2015)
-# df = preprocess.sort_dy_by_yr_continent(df)
+dhp_min = data['DHP'].min()
+dhp_max = data['DHP'].max()
 
-# fig = bubble.get_plot(df, gdp_range, co2_range)
-# fig = bubble.update_animation_hover_template(fig)
-# fig = bubble.update_animation_menu(fig)
-# fig = bubble.update_axes_labels(fig)
-# fig = bubble.update_template(fig)
-# fig = bubble.update_legend(fig)
+arrondissement = 'Le Plateau-Mont-Royal'
+especes = sorted(data['Essence_fr'].unique())
 
-# fig.update_layout(height=600, width=1000)
-# fig.update_layout(dragmode=False)
+nb_arbres_arrondissement = preprocess.get_nb_trees_district(data, date_plantation_min, date_plantation_max, dhp_min, dhp_max)
+missing_arrondissement = preprocess.get_missing_districts(nb_arbres_arrondissement, locations)
+data_arrondissement = preprocess.add_density(nb_arbres_arrondissement, 'assets/montreal.json')
+choropleth_fig = choropleth.get_choropleth(data_arrondissement, missing_arrondissement, montreal_data, densite=False)
+carte_arrond = arrond_map.getMap(data, arrondissement, 'Date_plantation', (None, None, None, None, None))
+
+bar_chart_ville = bar_chart.draw_bar_chart(data, None, 'Rue', True)
+bar_chart_arrond = bar_chart.draw_bar_chart(data, arrondissement, 'Rue', True)
+
+swarm = swarmplot.swarm(data)
+swarm_plot = swarmplot.swarmPlot(swarm)
 
 app.layout = html.Div(className='content', children=[
     html.Header(children=[
-        html.H1('Les arbres de Montréal'),
+        html.H1('Les arbres de la Ville de Montréal'),
     ]),
     html.Main(className='viz-container', children=[
         # dcc.Graph(className='graph', figure=fig, config=dict(
@@ -67,38 +79,181 @@ app.layout = html.Div(className='content', children=[
                 html.Div(id='date', children=[
                     html.H6('Date de plantation'),
                     dcc.RangeSlider(
-                        id='dateSlider',
+                        id='dateSlider', className='slider',
                         min=1960,
                         max=2023,
                         step=1,
                         value=[1960, 2023],
                         allowCross=False,  # Ensures the two handles do not cross each other
-                        marks=True,  # Hides the marks,
-                        tooltip={"placement": "bottom", "always_visible": True}
+                        marks=None,  # Hides the marks
+                        tooltip={
+                            "always_visible": True,
+                            "placement": "bottom",
+                            "style": {"color": "LightSteelBlue", "fontSize": "15px"},
+                        },
                     ),
                 ]),
                 html.Div(id='diametre', children=[
                     html.H6('Diamètre du tronc'),
                     dcc.RangeSlider(
-                        id='diametreSlider',
+                        id='diametreSlider', className='slider',
                         min=0,
                         max=300,
                         step=1,
                         value=[0, 300],
                         allowCross=False,  # Ensures the two handles do not cross each other
-                        marks=True,  # Hides the marks
-                        tooltip={"placement": "bottom", "always_visible": True}
+                        marks=None,  # Hides the marks
+                        tooltip={
+                            "always_visible": True,
+                            "placement": "bottom",
+                            "style": {"color": "LightSteelBlue", "fontSize": "15px"},
+                        },
                     ),
                 ])
                 
                 
             ]),
-            html.Div(id='total'),
-            html.Div(id='arrond'),
+            
+            html.Div(id='total', children=[
+                        # Div du choroplethe
+                        html.Div(id='div_critere_choropleth', children=[
+                            dcc.Dropdown(id='critere_choropleth',
+                                options=["Nombre d'arbres", "Densité d'arbres"],
+                                value="Nombre d'arbres",
+                                placeholder='Critère',
+                                multi=False,
+                                searchable=False,
+                                clearable=False)
+                        ],
+                        style={"margin" : "10px", "width": "50%"},),
+                        dcc.Graph(figure=choropleth_fig, id='choropleth',
+                            config=dict(
+                            scrollZoom=False))       
+            ]),
+
+            html.Div(id='arrond', children=[
+                        # Div de la carte de l'arrondissement
+                        html.Div(id='div_critere_carte_arrond', children=[
+                            dcc.Dropdown(id='critere_carte_arrond',
+                                options={"Date_plantation": "Date de plantation",
+                                         "Date_releve": "Date de relevé",
+                                         "DHP": "Diamètre du tronc"},
+                                value="Date_plantation",
+                                placeholder='Critère',
+                                multi=False,
+                                searchable=False,
+                                clearable=False)
+                        ],
+                        style={"margin" : "10px", "width": "50%"},),
+                        dcc.Graph(figure=carte_arrond, id='carte_arrond',
+                            config=dict(
+                            scrollZoom=True))       
+            ]),
         ]),
-        html.Div(id='swarm')
+        html.Div(id='barCharts', children=[
+            html.Div(id='divBarChartVille', children=[
+                        html.Div(id='div_critere_bar_chart_ville', children=[
+                            dcc.Dropdown(id='critere_bar_chart_ville',
+                                options={"Rue": "Rues",
+                                         "Emplacement": "Emplacements",
+                                         "Essence_fr": "Espèces"},
+                                value="Rue",
+                                placeholder='Critère',
+                                multi=False,
+                                searchable=False,
+                                clearable=False)
+                        ],
+                        style={"margin" : "10px", "width": "50%"},),
+                        dcc.Graph(figure=bar_chart_ville, id='barChartVille',
+                            config=dict(
+                            scrollZoom=False))       
+            ]),
+
+            html.Div(id='DivBarChartArrond', children=[
+                        html.Div(id='div_critere_bar_chart_arrond', children=[
+                            dcc.Dropdown(id='critere_bar_chart_arrond',
+                                options={"Rue": "Rues",
+                                         "Emplacement": "Emplacements",
+                                         "Essence_fr": "Espèces"},
+                                value="Rue",
+                                placeholder='Critère',
+                                multi=False,
+                                searchable=False,
+                                clearable=False)
+                        ],
+                        style={"margin" : "10px", "width": "50%"},),
+                        dcc.Graph(figure=bar_chart_arrond, id='barChartArrond',
+                            config=dict(
+                            scrollZoom=False))       
+            ]),
+        ]),
+        html.Div(id='swarm', children=[
+                    html.Div(id='div_espece_swarm', children=[
+                        dcc.Dropdown(id='espece_swarm',
+                            options=especes,
+                            placeholder='Espèce',
+                            multi=False,
+                            searchable=True,
+                            clearable=True)
+                        ], 
+                            style={"margin" : "10px", "width": "50%"}),
+                    dcc.Graph(figure=swarm_plot, id='swarm_plot',
+                        config=dict(
+                        scrollZoom=False))       
+        ])
     ])
 ])
+
+
+@app.callback(
+    Output('choropleth', 'figure'),
+    Output('carte_arrond', 'figure'),
+    Output('barChartVille', 'figure'),
+    Output('barChartArrond', 'figure'),
+    Input('choropleth', 'clickData'),
+    Input('critere_choropleth', 'value'),
+    Input('critere_carte_arrond', 'value'),
+    Input('dateSlider', 'value'),
+    Input('diametreSlider', 'value'),
+    Input('specie', 'value'),
+    Input('critere_bar_chart_ville', 'value'),
+    Input('critere_bar_chart_arrond', 'value'),
+    prevent_initial_call=True
+)
+def update_maps(clickData, critere_choropleth, critere_carte_arrond, date_range, dhp_range, species, critere_bar_chart_ville, critere_bar_chart_arrond):
+    global arrondissement
+    
+    densite = critere_choropleth == "Densité d'arbres"
+    nb_arbres_arrondissement = preprocess.get_nb_trees_district(data, pd.to_datetime(str(date_range[0]), format='%Y'), \
+                                                                pd.to_datetime(str(date_range[1] + 1), format='%Y'), \
+                                                                dhp_range[0], dhp_range[1], species)
+    missing_arrondissement = preprocess.get_missing_districts(nb_arbres_arrondissement, locations)
+    data_arrondissement = preprocess.add_density(nb_arbres_arrondissement, 'assets/montreal.json')
+    choropleth_updated = choropleth.get_choropleth(data_arrondissement, missing_arrondissement, montreal_data, densite=densite)
+        
+    ctx = dash.callback_context
+    trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if trigger_id == 'choropleth' and clickData is not None:
+        arrondissement = clickData['points'][0]['location']
+                
+    arrond_map_updated = arrond_map.getMap(data, arrondissement, critere_carte_arrond, (species, pd.to_datetime(str(date_range[0]), format='%Y'), pd.to_datetime(str(date_range[1] + 1), format='%Y'), dhp_range[0], dhp_range[1]))
+    
+    bar_chart_ville_updated = bar_chart.draw_bar_chart(data, None, critere_bar_chart_ville, True)
+    bar_chart_arrond_updated = bar_chart.draw_bar_chart(data, arrondissement, critere_bar_chart_arrond, True)
+    
+    return choropleth_updated, arrond_map_updated, bar_chart_ville_updated, bar_chart_arrond_updated
+
+
+@app.callback(
+    Output('swarm_plot', 'figure'),
+    Input('espece_swarm', 'value'),
+    prevent_initial_call=True
+)
+def update_swarm(espece_swarm):
+    swarm_plot_updated = swarmplot.swarmPlot(swarm, espece_swarm)
+    
+    return swarm_plot_updated
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ arrond_map.py
@@ -0,0 +1,59 @@
+import plotly.express as px
+from datetime import datetime
+
+def get_arrondissement_hover_template():
+    hover_template = (
+        '<b>Espèce</b> : <span font-weight: normal">%{customdata[0]}</span><br>' +
+        '<b>Date plantation</b> : <span font-weight: normal">%{customdata[1]}</span><br>' +
+        '<b>Date relevé</b> : <span font-weight: normal">%{customdata[2]}</span><br>' +
+        '<b>Diamètre tronc</b> : <span font-weight: normal">%{customdata[3]} cm</span><br>'
+    )
+    
+    return hover_template
+
+def getMap(data, arrondissement, critere, filter):
+    especes, min_date_plantation, max_date_plantation, min_dhp, max_dhp = filter
+    
+    # Sélectionner les arbres de l'arrondissement
+    data = data[data['ARROND_NOM'] == arrondissement]
+    
+    # Filtrer les données en fonction des espèces
+    if especes:
+        data = data[data['Essence_fr'].isin(especes)]
+    
+    # Filtrer les données en fonction des dates de plantation
+    if min_date_plantation != None and max_date_plantation != None:
+        data = data[(data['Date_plantation'] >= min_date_plantation) & (data['Date_plantation'] <= max_date_plantation)]
+    
+    # # Filtrer les données en fonction des dates de relevé
+    # if min_date_releve and max_date_releve:
+    #     data = data[(data['Date_releve'] >= min_date_releve) & (data['Date_releve'] <= max_date_releve)]
+        
+    # Filtrer les données en fonction des dates de relevé
+    if min_dhp != None and max_dhp != None:
+        data = data[(data['DHP'] >= min_dhp) & (data['DHP'] <= max_dhp)]
+    
+    # Légende de l'échelle de couleur    
+    if critere == 'Date_plantation':
+        title = 'Date de plantation'
+    elif critere == 'Date_releve':
+        title = 'Date de relevé'
+    else:
+        title = 'Diamètre du tronc (cm)'
+        
+    # Couleur des points en fonction du critère
+    color = data[critere].dt.year if critere in ['Date_plantation', 'Date_releve'] else data[critere]
+    
+    fig = px.scatter_mapbox(data, color=color, lat='Latitude', lon='Longitude', 
+                            zoom=12.5, color_continuous_scale='tempo', hover_data=['Essence_fr', 'Date_plantation_format', 'Date_releve_format', 'DHP'])
+    fig.update_layout(mapbox_style="open-street-map", coloraxis_colorbar=dict(title=title))
+    fig.update_layout(
+        coloraxis_colorbar_thickness=23,
+        margin=dict(l=60, r=60, t=60, b=60),
+    )
+    fig.update_traces(hovertemplate=get_arrondissement_hover_template())
+    # fig.show(config={'scrollZoom': True})
+    
+    return fig
+
+# getMap(clean, 'Le Plateau-Mont-Royal', 'Date_plantation', (['Érable argenté', 'Frêne de Pennsylvanie'], datetime(1950, 1, 1), datetime(2000, 12, 31), datetime(2019, 1, 1), datetime(2023, 12, 31)))
